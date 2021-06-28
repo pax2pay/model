fa21381e7c7a4d9c5d300770b34d10e49ca48a93
ChriJ <christofferj93@gmail.com>
2021-06-28 14:28:51 +0200

    Added necessary exports. (#10)
    Co-authored-by: Christoffer Johansson <christoffer@issuefab.com>

--- Purchase/index.ts
+++ Purchase/index.ts
@@ -6,6 +6,27 @@ export type Purchase = PurchaseCard | PurchaseTransfer
 
 export namespace Purchase {
 	export type Card = PurchaseCard
+	export namespace Card {
+		export type Operation = PurchaseCard.Operation
+		export namespace Operation {
+			export type Authorize = PurchaseCard.Operation.Authorize
+			export type Capture = PurchaseCard.Operation.Capture
+			export type Create = PurchaseCard.Operation.Create
+			export type Expire = PurchaseCard.Operation.Expire
+			export type Freeze = PurchaseCard.Operation.Freeze
+			export type Limit = PurchaseCard.Operation.Limit
+			export type Refund = PurchaseCard.Operation.Refund
+			export type Release = PurchaseCard.Operation.Release
+			export type Thaw = PurchaseCard.Operation.Thaw
+			export type Approved = PurchaseCard.Operation.Approved
+			export type Base = PurchaseCard.Operation.Base
+			export type Status = PurchaseCard.Operation.Status
+			export type Type = PurchaseCard.Operation.Type
+
+			export const balanceImpact = PurchaseCard.Operation.balanceImpact
+		}
+	}
+
 	export type Transfer = PurchaseTransfer
 
 	export type Type = PurchaseType
--- index.spec.ts
+++ index.spec.ts
@@ -1,3 +1,26 @@
-describe("library", () => {
-	it("true", () => expect(true).toBeTruthy())
+import * as model from "./index"
+
+describe("@pax2pay/model", () => {
+	it("model.Purchase.Card", () => {
+		const card: model.Purchase.Card = {
+			id: "test1234",
+			type: "card",
+			reference: { provider: "modulr", id: "1234qwer" },
+			organization: { name: "testname", code: "testcode", status: "active" },
+			amount: 111,
+			currency: "EUR",
+			meta: { testkey: "testvalue" },
+			operations: [],
+		}
+		expect(card).toEqual({
+			id: "test1234",
+			type: "card",
+			reference: { provider: "modulr", id: "1234qwer" },
+			organization: { name: "testname", code: "testcode", status: "active" },
+			amount: 111,
+			currency: "EUR",
+			meta: { testkey: "testvalue" },
+			operations: [],
+		})
+	})
 })
