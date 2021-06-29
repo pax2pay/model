a810b19a13506e255a04f622b7708b8a4a569c05
ChriJ <christofferj93@gmail.com>
2021-06-29 15:57:00 +0200

    Moved export to correct namespace. (#18)
    Co-authored-by: Christoffer Johansson <christoffer@issuefab.com>

--- Purchase/index.ts
+++ Purchase/index.ts
@@ -25,14 +25,14 @@ export namespace Purchase {
 			export type Settle = PurchaseCard.Operation.Settle
 
 			export const balanceImpact = PurchaseCard.Operation.balanceImpact
-
-			export const getStatus = PurchaseCard.getStatus
-			export const getAmount = PurchaseCard.getAmount
 		}
 		export type Status = PurchaseCard.Status
 		export namespace Status {
 			export const is = PurchaseCard.Status.is
 		}
+
+		export const getStatus = PurchaseCard.getStatus
+		export const getAmount = PurchaseCard.getAmount
 	}
 
 	export type Transfer = PurchaseTransfer
