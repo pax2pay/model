a4edd8fc9d88aad15bcf015fa48b0aada8f69401
Qijie Yu <qijie@issuefab.com>
2021-10-18 16:14:08 +0200

    Added remove() and update() in user.ts

--- Client/User.ts
+++ Client/User.ts
@@ -7,4 +7,10 @@ export class User {
 	async list(): Promise<modelUser[] | gracely.Error> {
 		return await this.connection.get<modelUser[]>("user")
 	}
+	async remove(user: string): Promise<modelUser | gracely.Error> {
+		return await this.connection.delete<modelUser>(`user/${user}`)
+	}
+	async update(user: string, request: modelUser.Creatable): Promise<modelUser | gracely.Error> {
+		return await this.connection.put<modelUser>(`user/${user}`, request)
+	}
 }
