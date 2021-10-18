1068de9f9900e6a070d9e216b5175c373932bc83
qijieye <86956109+qijieye@users.noreply.github.com>
2021-10-18 16:46:04 +0200

    Added remove() and update() in user.ts (#26)
    * Added remove() and update() in user.ts
    
    * Update.

--- Client/User.ts
+++ Client/User.ts
@@ -1,10 +1,16 @@
 import * as gracely from "gracely"
-import { User as modelUser } from "../User"
+import * as model from "../User"
 import { Connection } from "./Connection"
 
 export class User {
 	constructor(private readonly connection: Connection) {}
-	async list(): Promise<modelUser[] | gracely.Error> {
-		return await this.connection.get<modelUser[]>("user")
+	async list(): Promise<model.User[] | gracely.Error> {
+		return await this.connection.get<model.User[]>("user")
+	}
+	async remove(user: string): Promise<model.User | gracely.Error> {
+		return await this.connection.delete<model.User>(`user/${user}`)
+	}
+	async update(user: model.User.Change): Promise<model.User | gracely.Error> {
+		return await this.connection.put<model.User>(`user/${user.user}`, user)
 	}
 }
