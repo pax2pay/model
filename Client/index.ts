1e32e16a61b15baaecef5462ec483945e680c027
Qijie Yu <qijie@issuefab.com>
2022-12-22 13:11:17 +0100

    fixed type error and upgrade dependencies.

--- Client/index.ts
+++ Client/index.ts
@@ -9,12 +9,12 @@ export class Client extends rest.Client<gracely.Error> {
 	readonly me = new Me(this.client)
 	readonly account = new Account(this.client)
 	readonly user = new User(this.client)
-	static create<T extends Record<string, any> = Record<string, never>>(
-		server?: string,
+	static create<T = Record<string, any>>(
+		url?: string,
 		key?: string,
-		load?: (client: http.Client) => T
+		load?: (connection: http.Client) => T
 	): Client & T {
-		const client = new http.Client<gracely.Error>(server, key)
+		const client = new http.Client<gracely.Error>(url, key)
 		const result = new Client(client)
 		if (load)
 			Object.assign(result, load(client))
